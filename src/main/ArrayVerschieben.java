3db4473115c107824314b689dd1586663df96a6a
RickSanchezDE <RickSanchezDE@vermutlich.net>
2018-02-17 19:06:04 +0100

    Vergessene Klasse der Versionsverwaltung hinzugefügt.

---
+++ src/main/ArrayVerschieben.java
@@ -0,0 +1,56 @@
+package main;
+
+public class ArrayVerschieben {
+
+
+	public static void nachRechts(int[] array) {
+		int ueberfluss = 0;
+		for (int i = array.length - 1; i >= 0; i--) {
+			if (i == 0) {
+
+				array[i] = ueberfluss;
+
+			} else {
+
+				if (i == array.length - 1) {
+
+					ueberfluss = array[i];
+				}
+
+				array[i] = array[i - 1];
+			}
+		}
+
+		for (int i = 0; i < 5; i++) {
+			System.out.print("[" + array[i] + "]");
+		}
+
+		System.out.println("\n");
+
+	}
+	
+	public static void nachLinks(int[] array) {
+		int ueberfluss = 0;
+		for(int i = 0; i <= array.length-1; i++) {
+			if(i == array.length-1) {
+				
+				array[i] = ueberfluss;
+				
+			} else {
+				
+				if(i==0) {
+					
+					ueberfluss = array[i];
+				}
+				
+				array[i] = array[i+1];
+			}
+		}
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.print("["+array[i]+"]");
+		}
+		
+		System.out.println("\n");
+	}
+}
